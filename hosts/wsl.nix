7fb3ea433f7a6503298b88f73d5eef8caf08fd26
aiotter <git@aiotter.com>
2024-04-25 12:41:46 +0900

    feat(wsl): Update config

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -1,7 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
-  wsl.enable = true;
+  wsl = {
+    enable = true;
+    docker-desktop.enable = true;
+
+    extraBin =
+      let
+        show-my-ip = pkgs.writeShellApplication {
+          name = "show-my-ip";
+          runtimeInputs = with pkgs; [ unixtools.ifconfig gnugrep gawk ];
+          text = ''
+            #!/bin/sh
+            ifconfig eth0 | grep 'inet ' | awk '{print $2}'
+          '';
+        };
+      in
+      [{ src = "${show-my-ip}/bin/show-my-ip"; }];
+  };
+
+  # environment.etc."wsl.conf".text = ''
+  #   [wsl2]
+  #   networkingMode = mirrored
+  # '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
