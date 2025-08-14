a6763668342bd5d732b18069e8d5fca7c166c380
aiotter <git@aiotter.com>
2025-08-14 17:30:38 +0900

    fix(pivy-agent): remaning socket file is not deleted on start

--- modules/pivy-agent/default.nix
+++ modules/pivy-agent/default.nix
@@ -29,7 +29,7 @@ in
           name = "pivy-agent";
           runtimeInputs = [ cfg.package ];
           text = ''
-            [[ -p "${cfg.socket}" ]] && rm "${cfg.socket}"
+            [[ -e "${cfg.socket}" ]] && rm "${cfg.socket}"
             pivy-agent -ig "${cfg.guid}" -a "${cfg.socket}"
           '';
         });
