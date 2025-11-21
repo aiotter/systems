69f3a3f5cc6233b744548ef724dfa6178e805203
aiotter <git@aiotter.com>
2025-11-21 15:29:45 +0900

    fix: remove deprecated activate-user script

--- flake.nix
+++ flake.nix
@@ -4,50 +4,55 @@
   inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
     nixpkgs.follows = "determinate/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mac-app-util = {
       url = "github:hraban/mac-app-util";
+      # https://github.com/hraban/mac-app-util/issues/39#issuecomment-3503946041
+      inputs.cl-nix-lite.url = "github:r4v3n6101/cl-nix-lite/url-fix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, darwin, determinate, mac-app-util, ... }:
+  outputs =
+    flakeInputs@{ self, nixpkgs, darwin, determinate, mac-app-util }:
     let
-      darwinSystems = with flake-utils.lib.system; [ x86_64-darwin aarch64-darwin ];
+      inherit (nixpkgs) lib;
+      darwinSystems = lib.systems.doubles.darwin;
     in
-    flake-utils.lib.eachSystem darwinSystems (system: rec {
-      darwinConfigurations.default = darwin.lib.darwinSystem
+    {
+      packages = lib.genAttrs darwinSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          darwinConfig = darwin.lib.darwinSystem {
+            inherit system;
+            modules = [
+              determinate.darwinModules.default
+              mac-app-util.darwinModules.default
+              ./.
+            ];
+            specialArgs = { inherit flakeInputs; };
+          };
+        in
         {
-          inherit system;
-          modules = [
-            determinate.darwinModules.default
-            mac-app-util.darwinModules.default
-            ./.
-          ];
-          specialArgs = { flakeInputs = inputs; };
-        };
+          default = darwinConfig.system;
 
-      packages.darwin-system = darwinConfigurations.default.system;
-      packages.switch = import ./utils/darwin-switch.nix {
-        pkgs = nixpkgs.legacyPackages.${system};
-        darwinConfiguration = darwinConfigurations.default;
-      };
+          darwinConfigurations.default = darwinConfig;
 
-      apps.darwin-help = {
-        type = "app";
-        program = "${packages.darwin-system}/sw/bin/darwin-help";
-      };
-      apps.darwin-option = {
-        type = "app";
-        program = "${packages.darwin-system}/sw/bin/darwin-option";
-      };
-      apps.darwin-rebuild = {
-        type = "app";
-        program = "${packages.darwin-system}/sw/bin/darwin-rebuild";
-      };
-    });
+          inherit (darwin.packages.${system}) darwin-option darwin-rebuild darwin-version darwin-uninstaller;
+
+          # switch = pkgs.writeShellScriptBin "switch" ''
+          #   sudo "${lib.getExe darwin-rebuild}" switch --flake "${self}#default" "$@"
+          # '';
+
+          switch = pkgs.writeShellScriptBin "switch" ''
+            sudo "${darwinConfig.system}/sw/bin/darwin-rebuild" activate "$@"
+          '';
+        }
+      );
+    };
 }
