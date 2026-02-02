90be9374fe9e242e44e4a02802d2827a0f783738
aiotter <git@aiotter.com>
2026-02-02 21:46:06 +0900

    feat: add deskpad

--- homebrew-casks.nix
+++ homebrew-casks.nix
@@ -6,6 +6,10 @@ with pkgs.brewCasks;
 
 {
   home.packages = lib.optionals pkgs.stdenv.isDarwin [
+    # A virtual monitor for screen sharing
+    deskpad
+
+    # An open-source keystroke visualizer
     keycastr
   ];
 }
